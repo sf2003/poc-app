c135b71d3a9a2f97134c2c60f2e2fca26035df7d
zhubj <zhubj@liandisys.com.cn>
2018-09-04 09:27:13 +0800

    first upload

---
+++ src/main/java/com/rayfay/bizcloud/apps/pocapp/client/ExtractClient.java
@@ -0,0 +1,29 @@
+package com.rayfay.bizcloud.apps.pocapp.client;
+
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "poc-service")
+public interface ExtractClient {
+    @RequestMapping(method = {RequestMethod.GET}, value = "/user/findUserById")
+    JSONObject findUserById(@RequestParam(name = "userId") Long userId);
+
+    @RequestMapping(method = {RequestMethod.POST}, value = "/user/createUser")
+    JSONObject createUser(@RequestBody JSONObject user);
+
+    @RequestMapping(method = {RequestMethod.GET}, value = "/user/deleteUser")
+    JSONObject deleteUser(@RequestParam(name = "userId") Long userId);
+
+    @RequestMapping(method = {RequestMethod.POST}, value = "/user/updateUser")
+    JSONObject updateUser(@RequestBody JSONObject user);
+
+    @RequestMapping(method = {RequestMethod.POST}, value = "/user/findUserPageByQuery")
+    JSONObject findUserPageByQuery(@RequestParam(name = "pageSize", required = false) int pageSize,
+                                   @RequestParam(name = "pageNumber", required = false) int pageNumber,
+                                   @RequestBody JSONObject queryObject);
+
+}
